c5bf56c11814fd1a50f1caf5de13d559d184ed76
Felix Jones <felix@felixjones.co.uk>
2018-01-19 21:01:31 +0000

    Cleaned up AndroidInterface a bit.

--- app/src/main/java/systems/altimit/rpgmakermv/AndroidInterface.java
+++ app/src/main/java/systems/altimit/rpgmakermv/AndroidInterface.java
@@ -1,25 +1,32 @@
+/*
+ * Copyright (c) 2017-2018 Altimit Community Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or imp
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package systems.altimit.rpgmakermv;
 
-import android.content.Context;
 import android.net.Uri;
 import android.webkit.JavascriptInterface;
-import android.widget.Toast;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by felixjones on 19/01/2018.
@@ -30,11 +37,6 @@ public class AndroidInterface {
 
     public AndroidInterface(Player player) {
         mPlayer = player;
-    }
-
-    @JavascriptInterface
-    public void toast(String message) {
-        Toast.makeText(mPlayer.getContext(), message, Toast.LENGTH_SHORT).show();
     }
 
     @JavascriptInterface
